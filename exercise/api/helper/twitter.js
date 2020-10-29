f8f0d6fbca4807689b23ef42d8e61a50690c4c0c
vinitaran <vinitarane1998@gmail.com>
2020-10-29 20:10:21 +0530

    Added twitter class and dotenv file

---
+++ exercise/api/helper/twitter.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+const URL = 'https://api.twitter.com/1.1/search/tweets.json';
+class Twitter{
+    get(query,count){
+        return axios.get(URL,{
+            params: {
+                q:query,
+                count:count
+            },
+            headers: {
+                "Authorization":`Bearer ${process.env.TWITTER_API_TOKEN}`,
+            }
+        })
+    }
+}
+
+module.exports = Twitter;
--- exercise/app.js
+++ exercise/app.js
@@ -1,31 +1,21 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
+const Twitter = require('./api/helper/twitter');
+const twitter = new Twitter;
+require('dotenv').config();
 
 //routes
 app.get('/tweets', (req,res) => {
     const query = req.query.q;
     const count = req.query.count;
-    const URL = 'https://api.twitter.com/1.1/search/tweets.json';
-    axios.get(URL,{
-        params: {
-            q:query,
-            count:count
-        },
-        headers: {
-            "Authorization":"Bearer AAAAAAAAAAAAAAAAAAAAAGzUJAEAAAAAnHRQ55chxA%2Fyv%2BY3xvuyX1uPQBU%3D4aHjVhyHROZFbDbSgjauUf4LR4MI4mGwBvJgWmnEPJZGPPeu2Z",
-        }
-    }).then((response) => {
+    twitter.get(query,count).then((response) => {
         res.status(200).send(response.data);
     }).catch((error) => {
         res.status(400).send(error);
 
     })
+    
 })
-
-
-
-
 
 //setting up the server
 app.listen(3000,console.log('listening on port 3000'));
