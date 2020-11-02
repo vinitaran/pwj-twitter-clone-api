dd3ba088c59c01730d2e79db2009047c6514a051
vinitaran <vinitarane1998@gmail.com>
2020-11-03 01:21:36 +0530

    heroku

--- exercise/api/helper/twitter.js
+++ exercise/api/helper/twitter.js
@@ -2,11 +2,12 @@ const axios = require('axios');
 
 const URL = 'https://api.twitter.com/1.1/search/tweets.json';
 class Twitter{
-    get(query,count){
+    get(query,count,maxId){
         return axios.get(URL,{
             params: {
                 q:query,
-                count:count
+                count:count,
+                max_id:maxId
             },
             headers: {
                 "Authorization":`Bearer ${process.env.TWITTER_API_TOKEN}`,
--- exercise/app.js
+++ exercise/app.js
@@ -4,11 +4,20 @@ const Twitter = require('./api/helper/twitter');
 const twitter = new Twitter;
 require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
+
 //routes
 app.get('/tweets', (req,res) => {
     const query = req.query.q;
     const count = req.query.count;
-    twitter.get(query,count).then((response) => {
+    const maxId = req.query.max_id;
+    twitter.get(query,count,maxId).then((response) => {
         res.status(200).send(response.data);
     }).catch((error) => {
         res.status(400).send(error);
@@ -18,4 +27,4 @@ app.get('/tweets', (req,res) => {
 })
 
 //setting up the server
-app.listen(3000,console.log('listening on port 3000'));
+app.listen(PORT,console.log(`listening on port ${PORT}`));
